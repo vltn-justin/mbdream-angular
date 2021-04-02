e01a44df4349a2987a0acbc9bed50f4d412d2700
Valentin <vltn.justin@gmail.com>
2021-04-02 12:28:41 +0200

    Get moto list, save moto into service if you go to moto with motolist

--- src/app/components/moto/moto-list/moto-list.component.ts
+++ src/app/components/moto/moto-list/moto-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MotoService} from '../../../services/moto.service';
+import {MotoModel} from '../../../models/moto-model';
 
 @Component({
   selector: 'app-moto-list',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotoListComponent implements OnInit {
 
-  constructor() { }
+  motoList: MotoModel[];
+
+  constructor(private service: MotoService) { }
 
   ngOnInit(): void {
+    this.service.getAllMoto().subscribe(res => {
+      this.motoList = res;
+    });
   }
 
+  /**
+   * Method to save one moto into service
+   * @param moto Moto to save
+   */
+  saveMoto(moto: MotoModel): void {
+    this.service.saveOneMoto(moto);
+  }
 }
