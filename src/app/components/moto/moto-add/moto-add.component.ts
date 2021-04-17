c727410e6708a4bae4646054e2f8bd087bc91be5
Valentin <vltn.justin@gmail.com>
2021-04-17 20:59:20 +0200

    Make moto-add, with first style try

--- src/app/components/moto/moto-add/moto-add.component.ts
+++ src/app/components/moto/moto-add/moto-add.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, Validators} from '@angular/forms';
+import {MotoService} from '../../../services/moto.service';
+import {MotoForm} from '../../../models/moto-model';
+import {MarqueService} from '../../../services/marque.service';
+import {MarqueModel} from '../../../models/marque-model';
+import {CategorieService} from '../../../services/categorie.service';
+import {CategorieModel} from '../../../models/categorie-model';
 
 @Component({
   selector: 'app-moto-add',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotoAddComponent implements OnInit {
 
-  constructor() { }
+  errorMsg: string;
+  statusMsg: string[];
+
+  marques: MarqueModel[];
+  categories: CategorieModel[];
+
+  addMotoForm = this.formBuilder.group({
+    nomMoto: new FormControl('', [Validators.required]),
+    descriptionMoto: new FormControl('', [Validators.required]),
+    slugMarque: new FormControl('', [Validators.required]),
+    slugCategorie: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private formBuilder: FormBuilder, private serviceMoto: MotoService, private marqueService: MarqueService, private categorieService: CategorieService) { }
 
   ngOnInit(): void {
+    // Get all marques for form
+    this.marqueService.getAllMarques().subscribe(res => {
+      this.marques = res;
+    });
+
+    // Get all categories
+    this.categorieService.getAllCategories().subscribe(res => {
+      this.categories = res;
+    });
   }
 
+  addingMoto(): void {
+    // MotoForm declared inside MotoModel
+    const newMoto = new MotoForm(this.addMotoForm.get('nomMoto').value,
+      this.addMotoForm.get('descriptionMoto').value,
+      false,
+      this.addMotoForm.get('slugMarque').value,
+      this.addMotoForm.get('slugCategorie').value);
+
+    this.serviceMoto.addMoto(newMoto).subscribe(res => {
+      this.statusMsg = res.split('- ');
+    });
+  }
+
+  // Can use nomMoto, ... inside view instead of this.addMotoForm.get()
+  get nomMoto(): AbstractControl { return this.addMotoForm.get('nomMoto'); }
+  get descriptionMoto(): AbstractControl { return this.addMotoForm.get('descriptionMoto'); }
+  get slugMarque(): AbstractControl { return this.addMotoForm.get('slugMarque'); }
+  get slugCategorie(): AbstractControl { return this.addMotoForm.get('slugCategorie'); }
 }
