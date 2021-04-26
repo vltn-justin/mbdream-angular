3b953939c60b0eabd00597296dfbfe681f999a66
Valentin <vltn.justin@gmail.com>
2021-04-26 16:47:20 +0200

    Add quill editor and view to show and edit description

--- src/app/components/moto/moto/description/description.component.ts
+++ src/app/components/moto/moto/description/description.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {MotoModel} from '../../../../models/moto-model';
+import {Component, Input, OnInit} from '@angular/core';
+import {MotoForm, MotoModel} from '../../../../models/moto-model';
 import {MotoService} from '../../../../services/moto.service';
+import {QuillModule} from 'ngx-quill';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-description',
@@ -11,10 +13,42 @@ export class DescriptionComponent implements OnInit {
 
   moto: MotoModel;
 
+  onEdit = false;
+  @Input() control: FormControl;
+
+  errorMsg: string;
+  statusMsg: string;
+
   constructor(private motoService: MotoService) {
   }
 
   ngOnInit(): void {
     this.moto = this.motoService.getSavedMoto();
+    this.control = this.control ?? new FormControl(this.moto.descriptionMoto, [Validators.required]);
+  }
+
+  /**
+   * Method to show editor to edit desc or not
+   */
+  setEditorDesc(): void {
+    this.onEdit = !this.onEdit;
+  }
+
+  /**
+   * Save new desc
+   */
+  saveDesc(): void {
+    const updatedMoto = new MotoForm(this.moto.nomMoto,
+      this.control.value,
+      false,
+      this.moto.marque.slugMarque,
+      this.moto.categorie.slugCategorie,
+      this.moto.slugMoto);
+
+    this.motoService.updateMoto(updatedMoto).subscribe(res => {
+      this.statusMsg = res;
+    }, error => {
+      this.errorMsg = error.error.text;
+    });
   }
 }
