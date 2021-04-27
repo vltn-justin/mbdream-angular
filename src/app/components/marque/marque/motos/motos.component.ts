ba01dc28ec02377f1e98fc4783c96d0f4f46029b
Valentin <vltn.justin@gmail.com>
2021-04-27 12:26:35 +0200

    Create description, infos & motos components for marque

---
+++ src/app/components/marque/marque/motos/motos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motos',
+  templateUrl: './motos.component.html',
+  styleUrls: ['./motos.component.sass']
+})
+export class MotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
