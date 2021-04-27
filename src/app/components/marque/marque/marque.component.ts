974f50e6f2fbc6d0298e1fac3ccbf75d2c478ccd
Valentin <vltn.justin@gmail.com>
2021-04-27 12:28:07 +0200

    Get data, create view and set style

--- src/app/components/marque/marque/marque.component.ts
+++ src/app/components/marque/marque/marque.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ComponentFactoryResolver, ComponentRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {MarqueService} from '../../../services/marque.service';
+import {MarqueModel} from '../../../models/marque-model';
+import {ActivatedRoute, Router} from '@angular/router';
+import {faAlignLeft, faInfo, faMotorcycle} from '@fortawesome/free-solid-svg-icons';
+import {DescriptionComponent} from './description/description.component';
+import {InfosComponent} from './infos/infos.component';
+import {MotosComponent} from './motos/motos.component';
 
 @Component({
   selector: 'app-marque',
@@ -7,9 +14,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarqueComponent implements OnInit {
 
-  constructor() { }
+  faDesc = faAlignLeft;
+  faInfo = faInfo;
+  faMoto = faMotorcycle;
 
-  ngOnInit(): void {
+  marque: MarqueModel;
+  slug: string;
+  backgroundIMG = '';
+
+  tabSelector: string;
+
+  @ViewChild('contentTabMarque', {read: ViewContainerRef, static: true}) container: ViewContainerRef;
+
+  private componentRef: ComponentRef<{}>;
+  private componentsMapping = {
+    description: DescriptionComponent,
+    infos: InfosComponent,
+    motos: MotosComponent,
+  };
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private marqueService: MarqueService,
+              private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
+  ngOnInit(): void {
+    // Path Variable; Slug
+    this.slug = this.route.snapshot.params.slug;
+
+    this.getOneMarque(this.slug);
+
+    // Default tab is Description
+    this.tabSelector = 'description';
+  }
+
+  /**
+   * Method to get one marque from back
+   * @param slugMarque Slug of marque
+   */
+  getOneMarque(slugMarque: string): void {
+    this.marqueService.getOneMarque(slugMarque).subscribe(res => {
+      this.marque = res;
+      // On charge l'image background
+      this.loadImg();
+      // Partage des données avec les components enfant
+      this.marqueService.saveOneMarque(this.marque);
+      // Dynamic component loader for tabs
+      this.changeContent();
+    }, error => {
+      this.router.navigate(['/404']).then(null);
+    });
+  }
+
+  /**
+   * Method to set background img
+   */
+  loadImg(): void {
+    if (this.marque.logoMarque !== null) {
+      this.backgroundIMG = this.marque.logoMarque;
+    }
+  }
+
+  /**
+   * Method to change content displayed inside contentTabMoto
+   */
+  changeContent(): void {
+    this.container.clear();
+    const component = this.componentsMapping[this.tabSelector];
+    const factory = this.componentFactoryResolver.resolveComponentFactory(component);
+    this.componentRef = this.container.createComponent(factory);
+  }
 }
