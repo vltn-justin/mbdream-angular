bc6de463e70acc794022007fa14425e7bc7b9473
Valentin <vltn.justin@gmail.com>
2021-04-27 11:40:35 +0200

    Define form & methods to add a new marque

--- src/app/components/marque/marque-add/marque-add.component.ts
+++ src/app/components/marque/marque-add/marque-add.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, Validators} from '@angular/forms';
+import {MarqueService} from '../../../services/marque.service';
+import {MarqueForm} from '../../../models/marque-model';
+import {imgValidator} from '../../../validator/img-validator';
 
 @Component({
   selector: 'app-marque-add',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarqueAddComponent implements OnInit {
 
-  constructor() { }
+  errorMsg: string;
+  statusMsg: string[];
+
+  addMarqueForm = this.formBuilder.group({
+    nomMarque: new FormControl('', [Validators.required]),
+    descriptionMarque: new FormControl('', [Validators.required]),
+    dateMarque: new FormControl('', [Validators.required]),
+    logoMarque: new FormControl('', [Validators.required])
+  });
+
+  constructor(private formBuilder: FormBuilder, private marqueService: MarqueService) {
+  }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Method to add a Marque
+   */
+  addingMarque(): void {
+    if (imgValidator(this.logoMarque.value)) {
+      const newMarque = new MarqueForm(
+        this.nomMarque.value,
+        this.descriptionMarque.value,
+        this.dateMarque.value,
+        this.logoMarque.value
+      );
+
+      this.marqueService.addMarque(newMarque).subscribe(res => {
+        this.statusMsg = res.split('- ');
+      }, error => {
+        this.errorMsg = error.error.text;
+      });
+    } else {
+      this.logoMarque.setErrors({format: true});
+    }
+  }
+
+  // Can use nomMoto, ... inside view instead of this.addMarqueForm.get()
+  get nomMarque(): AbstractControl {
+    return this.addMarqueForm.get('nomMarque');
+  }
+
+  get descriptionMarque(): AbstractControl {
+    return this.addMarqueForm.get('descriptionMarque');
+  }
+
+  get dateMarque(): AbstractControl {
+    return this.addMarqueForm.get('dateMarque');
+  }
+
+  get logoMarque(): AbstractControl {
+    return this.addMarqueForm.get('logoMarque');
+  }
 }
