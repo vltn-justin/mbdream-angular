8e2b1cd221a959ad20c2eda04631a6501c916723
Valentin <vltn.justin@gmail.com>
2021-04-30 09:56:53 +0200

    Display description, and can edit

--- src/app/components/marque/marque/description-marque/description-marque.component.ts
+++ src/app/components/marque/marque/description-marque/description-marque.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MarqueModel} from '../../../../models/marque-model';
+import {MarqueService} from '../../../../services/marque.service';
+import {AbstractControl, FormBuilder, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-description-marque',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescriptionMarqueComponent implements OnInit {
 
-  constructor() { }
+  marque: MarqueModel;
+
+  errorMsg: string;
+  successMsg: string;
+
+  onEdit = false;
+  firstEdit = true;
+
+  descriptionForm = this.formBuilder.group({
+    descriptionMoto: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private marqueService: MarqueService, private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.marque = this.marqueService.getSavedMarque();
   }
 
+  /**
+   * Method to show editor to edit desc or not
+   */
+  setEditorDesc(): void {
+    this.onEdit = !this.onEdit;
+
+    if (this.firstEdit) {
+      this.descriptionMoto.setValue(this.marque.descriptionMarque);
+      this.firstEdit = false;
+    }
+  }
+
+  /**
+   * Save new desc
+   */
+  saveDesc(): void {
+
+  }
+
+  get descriptionMoto(): AbstractControl { return this.descriptionForm.get('descriptionMoto'); }
 }
