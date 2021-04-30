b7738b9ab5b0f89a6c6dd0ff235ec4c0a47cd6d0
Valentin <vltn.justin@gmail.com>
2021-04-30 12:54:47 +0200

    Add getCategorie method

--- src/app/services/categorie.service.ts
+++ src/app/services/categorie.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {InfoModel} from '../models/info-model';
 import {environment} from '../../environments/environment';
 import {CategorieModel} from '../models/categorie-model';
 
@@ -18,5 +17,13 @@ export class CategorieService {
    */
   getAllCategories(): Observable<CategorieModel[]> {
     return this.http.get<CategorieModel[]>(environment.apiBaseUrl + '/category/get');
+  }
+
+  /**
+   * Method to get one category
+   * @param slug Slug of category
+   */
+  getCategorie(slug: string): Observable<CategorieModel> {
+    return this.http.get<CategorieModel>(environment.apiBaseUrl + '/category/get/' + slug);
   }
 }
